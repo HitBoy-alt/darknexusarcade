07b694dfa9239ea0a025277732577ec50935c927
HitBoy-alt <ggamer123888@gmail.com>
2025-09-22 13:26:50 -0400

    Update rizz.sw.js

--- hitboy/rizz.sw.js
+++ hitboy/rizz.sw.js
@@ -1,12 +1,6 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  // Simple passthrough proxy for all fetch requests
-  event.respondWith(fetch(event.request));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  // Simple passthrough fetch
+  event.respondWith(fetch(event.request).catch(() => new Response("Fetch failed", { status: 502 })));
 });
