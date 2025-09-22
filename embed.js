c70637638dbfde671fdc48f8dbe30db2eb1173e9
HitBoy-alt <ggamer123888@gmail.com>
2025-09-22 13:25:12 -0400

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,20 +1,19 @@
 "use strict";
 
-(async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   let destination = "";
 
   try {
     destination = new URL(location.hash.slice(1)).toString();
   } catch {
-    alert("Invalid URL in hash! Use format: embed.html#https://example.com");
+    alert("Invalid URL in hash! Example: embed.html#https://example.com");
     return;
   }
 
   try {
     await registerSW();
-
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(destination);
-  } catch (e) {
-    alert("Service worker registration or redirect failed: " + e);
+    location.href = __uv$config.prefix + __uv$config.encodeUrl(destination);
+  } catch (err) {
+    alert("Service worker registration or redirect failed:\n" + err);
   }
-})();
+});
