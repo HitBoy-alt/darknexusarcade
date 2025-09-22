5e5611a29092af846172da22511351b3c056591d
HitBoy-alt <ggamer123888@gmail.com>
2025-09-22 13:25:32 -0400

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -4,11 +4,13 @@ const swAllowedHostnames = ["localhost", "127.0.0.1"];
 const swPath = "/hitboy/rizz.sw.js";
 
 async function registerSW() {
-  if (!("serviceWorker" in navigator)) {
+  if (!navigator.serviceWorker) {
     if (location.protocol !== "https:" && !swAllowedHostnames.includes(location.hostname))
       throw new Error("Service workers require HTTPS.");
-    throw new Error("Service workers are not supported by this browser.");
+    throw new Error("Your browser does not support service workers.");
   }
 
-  await navigator.serviceWorker.register(swPath, { scope: __uv$config.prefix });
+  await navigator.serviceWorker.register(swPath, {
+    scope: __uv$config.prefix,
+  });
 }
