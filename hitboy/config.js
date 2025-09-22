7af8eebd9e45565ef5904d2dde6bc19758307548
HitBoy-alt <ggamer123888@gmail.com>
2025-09-22 13:33:25 -0400

    Update config.js

--- hitboy/config.js
+++ hitboy/config.js
@@ -1,13 +1,10 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
   prefix: "/hitboy/",
   bare: "/bare/",
-  
-  // Removed encoding functions
-  encodeUrl: (url) => url,
+
+  encodeUrl: (url) => url,     // No encoding
   decodeUrl: (url) => url,
-  
+
   handler: "/hitboy/handler.js",
   client: "/hitboy/client.js",
   bundle: "/hitboy/bundle.js",
